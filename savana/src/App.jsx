5a2714e77641ed61c657cf2a98d353fb966868d2
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-12 20:26:49 +0530

    add footer

--- savana/src/App.jsx
+++ savana/src/App.jsx
@@ -1,9 +1,12 @@
-
+import Header from "./Component/Header"
+import Footer from "./Component/Footer"
 
 function App() {
 
   return (
     <>
+      <Header />
+      {/* <Footer /> */}
     </>
   )
 }
