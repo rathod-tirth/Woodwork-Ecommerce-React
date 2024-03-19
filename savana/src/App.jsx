3e45bac267436dec9e4a8cb302a5154f8d01eed5
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-19 14:27:44 +0530

    add home page

--- savana/src/App.jsx
+++ savana/src/App.jsx
@@ -1,11 +1,13 @@
 import Header from "./Component/Header"
 import Footer from "./Component/Footer"
+import Home from "./Page/Home"
 
 function App() {
 
   return (
     <>
       <Header />
+      <Home />
       {/* <Footer /> */}
     </>
   )
