03823bbc44d128027a76da5341e3955c16d64f9b
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-28 18:12:48 +0530

    add navlink

--- savana/src/Component/Header.jsx
+++ savana/src/Component/Header.jsx
@@ -1,3 +1,4 @@
+import { Link, NavLink } from 'react-router-dom'
 import userSVG from '../assets/images/user.svg'
 import cartSVG from '../assets/images/cart.svg'
 import ScrollToTop from './ScrollToTop'
@@ -8,15 +9,13 @@ const Header = () => {
          {/* Start Header/Navigation */}
          <nav className="custom-navbar navbar navbar navbar-expand-md navbar-dark bg-dark" arial-label="Woodwork navigation bar">
             <div className="container">
-               <a className="navbar-brand" href="/">Woodwork<span>.</span></a>
+               <Link className="navbar-brand" to="/">Woodwork<span>.</span></Link>
                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsWoodwork" aria-controls="navbarsWoodwork" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon" />
                </button>
                <div className="collapse navbar-collapse" id="navbarsWoodwork">
                   <ul className="custom-navbar-nav navbar-nav ms-auto mb-2 mb-md-0">
-                     <li className="nav-item active">
-                        <a className="nav-link" href="/">Home</a>
-                     </li>
+                     <li><NavLink className="nav-link active" to="/">Home</NavLink></li>
                      <li><a className="nav-link" href="shop.html">Shop</a></li>
                      <li><a className="nav-link" href="about.html">About us</a></li>
                      <li><a className="nav-link" href="services.html">Services</a></li>
