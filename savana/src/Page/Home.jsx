b80baf4b1f4cccd78b203534480403a94a7619fb
Tirth Rathod <tirthrathod05@gmail.com>
2024-03-31 18:55:19 +0530

    update home

--- savana/src/Page/Home.jsx
+++ savana/src/Page/Home.jsx
@@ -167,58 +167,6 @@ function Home() {
                </div>
             </div>
             {/* End Product Section */}
-            {/* Start Popular Section */}
-            <div className="popular-section product-section">
-               <div className="container">
-                  <div className="row">
-                     {/* Start Column 2 */}
-                     <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <a className="product-item" href="cart.html">
-                           <img src={product1PNG} className="img-fluid product-thumbnail" />
-                           <h3 className="product-title">Nordic Chair</h3>
-                           <strong className="product-price">$50.00</strong>
-                           <span className="icon-cross">
-                              <img src={crossSVG} className="img-fluid" />
-                           </span>
-                        </a>
-                     </div>
-                     {/* End Column 2 */}
-                     {/* Start Column 3 */}
-                     <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <a className="product-item" href="cart.html">
-                           <img src={product2PNG} className="img-fluid product-thumbnail" />
-                           <h3 className="product-title">Kruzo Aero Chair</h3>
-                           <strong className="product-price">$78.00</strong>
-                           <span className="icon-cross">
-                              <img src={crossSVG} className="img-fluid" />
-                           </span>
-                        </a>
-                     </div>
-                     {/* End Column 3 */}
-                     {/* Start Column 4 */}
-                     <div className="col-12 col-md-4 col-lg-3 mb-5 mb-md-0">
-                        <a className="product-item" href="cart.html">
-                           <img src={product3PNG} className="img-fluid product-thumbnail" />
-                           <h3 className="product-title">Ergonomic Chair</h3>
-                           <strong className="product-price">$43.00</strong>
-                           <span className="icon-cross">
-                              <img src={crossSVG} className="img-fluid" />
-                           </span>
-                        </a>
-                     </div>
-                     {/* End Column 4 */}
-                     {/* Start Column 1 */}
-                     <div className="col-md-12 col-lg-3 mb-5 mb-lg-0">
-                        <h2 className="mb-4 section-title">Popular Items</h2>
-                        <p className="mb-4">Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate
-                           velit imperdiet dolor tempor tristique. </p>
-                        <p><a href="shop.html" className="btn">Explore</a></p>
-                     </div>
-                     {/* End Column 1 */}
-                  </div>
-               </div>
-            </div>
-            {/* End Popular Section */}
             {/* Start Why Choose Us Section */}
             <div className="why-choose-section">
                <div className="container">
